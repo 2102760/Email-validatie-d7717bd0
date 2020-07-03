4a1fc1271d1dcf8a8e18e28fcb7418198b50c49e
Beau van Rouwendal <2102760@talnet.nl>
2020-07-03 14:51:41 +0200

    uitwerking

--- guestbook.php
+++ guestbook.php
@@ -69,7 +69,7 @@ $token = $_SESSION['token'];
         $email = $_POST['email'];
 
         $text = $_POST['text'];
-        $admin = isset($_POST['admin']) ? 1 : 0;
+        $admin = userIsAdmin($conn) ? 1 : 0;
         if (userIsAdmin($conn)) {
             $color = $_POST['color'];
         } else {
